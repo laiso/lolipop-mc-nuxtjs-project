ac9076420d5d720d8a333b3d124f4e2fd6907bf5
laiso <laiso@lai.so>
2018-05-17 18:32:02 +0700

    nuxtize

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const { Nuxt } = require('nuxt-start')
 
-var app = express();
+const config = require('./nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+const nuxt = new Nuxt(config)
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express()
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 var expressVersion = require('./node_modules/express/package.json').version;
 app.set('express-version', expressVersion);
 
@@ -23,10 +19,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(nuxt.render)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +35,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
